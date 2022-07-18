97379b266b615fdc8e8526c0b2d115aad6dd001b
Asanda <noelmajola@gmail.com>
2022-07-18 11:19:07 +0200

    Modified file

--- index.js
+++ index.js
@@ -59,12 +59,12 @@ function searchFilter() {
 document.getElementById("searcher").addEventListener("input", searchFilter);
 
 //A function that will filter content based on the currently selected option in the dropdown lists
-function optionsFilter() {
-  let selectTagsValue = document.querySelector("select").value;
+function minimumPriceFilter() {
+  let minPriceValue = document.getElementById("min-price").value;
   let cards = document.querySelectorAll(".card");
   for (let i = 0; i < cards.length; i++) {
     if (
-      cards[i].textContent.toLowerCase().includes(selectTagsValue.toLowerCase())
+      cards[i].textContent.toLowerCase().includes(minPriceValue.toLowerCase())
     ) {
       cards[i].classList.remove("hidden");
     } else {
@@ -73,4 +73,60 @@ function optionsFilter() {
   }
 }
 
-document.querySelector("select").addEventListener("change", optionsFilter);
+document
+  .getElementById("min-price")
+  .addEventListener("change", minimumPriceFilter); // the change event will listen for any 'change in value' made to the minimum price dropdown menu.
+
+function maximumPriceFilter() {
+  let maxPriceValue = document.getElementById("max-price").value;
+  let cards = document.querySelectorAll(".card");
+  for (let i = 0; i < cards.length; i++) {
+    if (
+      cards[i].textContent.toLowerCase().includes(maxPriceValue.toLowerCase())
+    ) {
+      cards[i].classList.remove("hidden");
+    } else {
+      cards[i].classList.add("hidden");
+    }
+  }
+}
+
+document
+  .getElementById("max-price")
+  .addEventListener("change", maximumPriceFilter); // the change event will listen for any 'change in value',which are options selected by the user, to the maximum price dropdown menu.
+
+function locationFilter() {
+  let selectedLocation = document.getElementById("Location").value;
+  let cards = document.querySelectorAll(".card");
+  for (let i = 0; i < cards.length; i++) {
+    if (
+      cards[i].textContent
+        .toLowerCase()
+        .includes(selectedLocation.toLowerCase())
+    ) {
+      cards[i].classList.remove("hidden");
+    } else {
+      cards[i].classList.add("hidden");
+    }
+  }
+}
+
+document.getElementById("Location").addEventListener("change", locationFilter); // the change event will listen for any 'change in value',which are options selected by the user, to the location dropdown menu.
+
+function bodyTypeFilter() {
+  let selectedBodyType = document.getElementById("Body-type").value;
+  let cards = document.querySelectorAll(".card");
+  for (let i = 0; i < cards.length; i++) {
+    if (
+      cards[i].textContent
+        .toLowerCase()
+        .includes(selectedBodyType.toLowerCase())
+    ) {
+      cards[i].classList.remove("hidden");
+    } else {
+      cards[i].classList.add("hidden");
+    }
+  }
+}
+
+document.getElementById("Body-type").addEventListener("change", bodyTypeFilter); // the change event will listen for any 'change in value', which are options selected by the user, to the body-type dropdown menu.
