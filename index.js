9ef76ac8f92f441e5b8d75ac7207ea6005854b9c
Asanda <noelmajola@gmail.com>
2022-09-13 20:32:18 +0200

    Create intersection observer for every vehicle card listing.
    This code is responsible for asynchronously adding animation class to each vehicle card listing when the viewport intersects with each card element.

--- index.js
+++ index.js
@@ -121,14 +121,31 @@ function bodyTypeFilter() {
 
 document.getElementById("Body-type").addEventListener("change", bodyTypeFilter); // the change event will listen for any 'change in value', which are options selected by the user, to the body-type dropdown menu.
 
-const vehichlesForSale = document.querySelectorAll(".card.m-2");
+//*selecting the target elements for our observer API
+const targetElements = document.querySelectorAll(".card.m-2");
 
-/*Converting the 'vehiclesForSale' from an HTMLcollection into an array for iteration purposes.
-Thereafter, addinng an animation class for each element*/
+//* constructing our intersectionObserverAPI object
+const options = {
+  root: null, // the root element when set to null allows the browser to use the document's viewport to watch for intersections.
+  rootMargin: "0px",
+  threshold: 0, // specifies the distance between the root element and the target element. 0 means as soon as a single pixel of the target element becomes visible in the document's viewport , an event needs to be triggered.
+};
 
-Array.from(vehichlesForSale).forEach(function (vehichleForSale) {
-  return vehichleForSale.classList.add(
-    "animate__animated",
-    "animate__fadeInLeft"
-  );
+const handleIntersection = (entries) => {
+  entries.forEach((entry) => {
+    if (entry.isIntersecting) {
+      entry.target.classList.add("animate__animated", "animate__fadeInLeft");
+    }
+  });
+};
+const observer = new IntersectionObserver(handleIntersection, options);
+
+/*
+ The below code watches for changes between the root element and each target element.
+It is important to note that our observe method watches a single element at a time , 
+therefore we have to loop through our array of target elements.
+*/
+
+targetElements.forEach((targetElement) => {
+  observer.observe(targetElement);
 });
